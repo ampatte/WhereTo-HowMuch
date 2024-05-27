34b78f5c02a9e335caf34c82db98e8a0e36c47b9
Anna Marie Patterson <ampatterson1@hotmail.com>
2024-05-27 08:06:27 -0400

    created files, added stumps for tests

---
+++ currency.py
@@ -0,0 +1,15 @@
+"""
+Module for currency exchange
+
+This module provides several string parsing functions to implement a simple
+currency exchange routine using an online currency service. The primary function
+in this module is exchange().
+
+Author: Anna Marie Patterson
+Date:   May 24, 2024
+"""
+
+
+import introcs
+import important
+api_key = important.passkey
---
+++ testcurrency.py
@@ -0,0 +1,80 @@
+"""
+Unit tests for module currency
+
+When run as a script, this module invokes several procedures that test
+the various functions in the module currency.
+
+Author: Anna Marie Patterson
+Date:   May 24, 2024
+"""
+
+
+import introcs
+import currency
+
+def test_before_space():
+    """Test procedure for before_space"""
+    
+    print('Testing_before_space')
+
+
+def test_after_space():
+    """Test procedure for after_space"""
+    
+    print('Testing_after_space')
+
+
+def first_inside_quotes():
+    """Test procedure for first_inside_quotes"""
+    
+    print('first_inside_quotes')
+
+
+def get_src():
+    """Test procedure for get_src"""
+
+    print('get_src')    
+
+
+def get_dst():
+    """Test procedure for get_dst"""
+
+    print('get_dst') 
+
+
+def has_error():
+    """Test procedure for has_error"""
+
+    print('has_error') 
+
+
+def service_response():
+    """Test procedure for service_response"""
+
+    print('service_response')
+
+
+def iscurrency():
+    """Test procedure for iscurrency"""
+
+    print('iscurrency')
+
+
+def exchange():
+    """Test procedure for exchange"""
+
+    print('exchange')
+
+
+test_before_space()
+test_after_space()
+test_first_inside_quotes()
+test_get_src()
+test_get_dst()
+test_has_error()
+test_service_response()
+test_iscurrency()
+test_exchange()
+
+
+print("All tests completed successfully.")
